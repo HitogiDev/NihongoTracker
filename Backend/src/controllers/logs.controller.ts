d8024fef8579c122a87d192099e7dd98ff9f776d
ElaxDev <eliamrarodriguez@outlook.com>
2025-08-08 09:17:29 -0400

    Fixes reading logs with only time not giving xp

--- Backend/src/controllers/logs.controller.ts
+++ Backend/src/controllers/logs.controller.ts
@@ -1445,9 +1445,11 @@ export async function recalculateXp(
             case 'reading':
             case 'manga':
               if (charsXp) {
-                log.xp = Math.max(charsXp, timeXp);
+                log.xp = Math.max(charsXp, timeXp, 0);
               } else if (pagesXp) {
-                log.xp = Math.max(pagesXp, timeXp);
+                log.xp = Math.max(pagesXp, timeXp, 0);
+              } else if (timeXp) {
+                log.xp = timeXp;
               } else {
                 log.xp = 0;
               }
