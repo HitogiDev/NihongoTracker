932ded9f7c63f4174fddc3f24c943567aa018a14
HitogiDev <31047609+HitogiDev@users.noreply.github.com>
2026-07-28 19:45:16 -0400

    feat: add Inter Variable font and remove Roboto font imports for improved typography

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,8 +1,5 @@
 import './index.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource-variable/inter';
 import 'react-image-crop/dist/ReactCrop.css';
 import 'react-toastify/dist/ReactToastify.css';
 import React, { Suspense, lazy } from 'react';
